7bc7327676c85f36783ed9767bae8fff28d87c9a
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2024-10-09 21:12:36 +0700

    # fix tailwind config conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-<<<<<<< HEAD
-=======
-    // "./app.{js,jsx,ts,tsx}",
->>>>>>> 44812034fda428ecbd0af075f91c9ab845801195
     "./app/**/*.{js,jsx,ts,tsx}",
     "./componets/**/*.{js,jsx,ts,tsx}"
   ],
