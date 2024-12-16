0b25035454de789d746789f925cb1a4deb7e8e54
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2024-12-16 23:13:54 +0700

    # feat infer auth

--- src/api/AiAPI.ts
+++ src/api/AiAPI.ts
@@ -1,11 +1,16 @@
 import axios from "./axios";
 
+const token = '10|cBz3Z2VjFbbAbllob6VvAa6n8YqDfclh9J7XKdgT9faf048c';
+
 const AiAPI = {
     infer: async(data: any) => {
         return await axios.request({
             url: "/ai",
             method: "POST",
             data,
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
         });
     },
     
