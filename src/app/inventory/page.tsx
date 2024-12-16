7cf4eb368877be410a404ab1bad231c47196e67c
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2024-12-16 13:51:08 +0700

    # feat server component for inventory

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,23 +1,16 @@
 import InventoryAPI from "@/api/InventoryAPI";
 import InventoryCard from "@/components/custom/inventory-card";
 import { InventoryItem } from "@/types";
-import { useEffect, useState } from "react";
 
-export default function Page() {
-  const [inventory, setInventory] = useState<InventoryItem[]>();
+export default async function Page() {
+  let inventory: InventoryItem[] = [];
 
-  const fetchInventory = async () => {
-    try {
-      const response = await InventoryAPI.all();
-      setInventory(response.data);
-    } catch (err: any) {
-      console.error("Server Error:", err.response.data);
-    }
+  try {
+    const response = await InventoryAPI.all();
+    inventory = response.data;
+  } catch (err: any) {
+    console.error("Server Error:", err.response.data);
   };
-
-  useEffect(() => {
-    fetchInventory();
-  }, []);
 
   return (
     <section className="w-full h-full flex justify-center">
