7064e2afde0fdd417af758910c7cc345aaf68cc6
Colin Oliver <84835385+SolidGluten@users.noreply.github.com>
2024-12-22 21:16:27 +0700

    #feat redirect from login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -17,15 +17,6 @@ export default function Page() {
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [remember, setRemember] = useState<boolean>(false);
-    // const [formData, setFormData] = useState({
-    //     username: "",
-    //     password: "",
-    // });
-
-    // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //     const { id, value } = e.target;
-    //     setFormData({...formData, [id]: value});
-    // }
 
     const handleSubmit =  useCallback(async (e: React.FormEvent) => {
         e.preventDefault();
@@ -40,9 +31,8 @@ export default function Page() {
         };
 
         try {
-            // console.log(formData);
             const response = await AuthAPI.login(data); 
-            // router.push("/")
+            router.push("/")
         } catch (err: any) {
             console.error("Server Error:", err.response.data);
             setError(err.response?.data?.message || "Invalid input");
@@ -87,13 +77,13 @@ export default function Page() {
                             className="w-full px-3 py-2 mt-1 border-0 rounded-lg bg-gray-100 shadow-sm"
                         />
                     </div>
-                    <div>
+                    <div className="flex items-center">
                         <Checkbox
                             name="remember"
                             checked={remember}
                             onCheckedChange={() => {setRemember(!remember)}}
                         />
-                        <Label htmlFor="remember" className="ml-2">Remember me</Label>
+                        <Label htmlFor="remember" className="ml-2 items-">Remember me</Label>
                     </div>
                     <Button 
                         variant="default"
